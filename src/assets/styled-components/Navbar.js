5b2057a7ea14c689ddcc862300d0d6af1271468d
Aksha-40007 <akshavad40007@gmail.com>
2024-04-29 00:58:29 +0530

    added dropdown for hamburger

--- src/assets/styled-components/Navbar.js
+++ src/assets/styled-components/Navbar.js
@@ -11,17 +11,11 @@ left:0;
 
 export const DropDownMenu = styled.div`
 position: absolute;
-width: 100%;
+width: 20%;
 top: 3rem;
-
 right: 0;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
 padding: 2rem;
 background-color: white;
 box-shadow: 0px 4px 4px 0px #00000040;
-padding: 2rem 0rem;
 z-index: 5;
 `;
