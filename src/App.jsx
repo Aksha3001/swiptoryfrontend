eb1dcb14b14335e99e2e0f30abbbfcbca407d7dd
Aksha-40007 <akshavad40007@gmail.com>
2024-04-29 01:08:13 +0530

    added dropdown for hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
